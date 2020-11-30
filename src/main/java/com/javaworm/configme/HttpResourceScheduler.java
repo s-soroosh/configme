ae58cbfe0b3da5a9f6e11441112abbf4943841ed
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2020-11-30 14:27:03 +0100

    run job on predefine interval

--- src/main/java/com/javaworm/configme/HttpResourceScheduler.java
+++ src/main/java/com/javaworm/configme/HttpResourceScheduler.java
@@ -6,44 +6,56 @@ import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
 import io.fabric8.kubernetes.api.model.DoneableConfigMap;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HttpResourceScheduler {
-    HttpClient client = HttpClient.newHttpClient();
-    KubernetesClient k8sClient;
+    private HttpClient client = HttpClient.newHttpClient();
+    private KubernetesClient k8sClient;
+    private Timer timer = new Timer(HttpResourceScheduler.class.getName());
+    private static final Logger log = LoggerFactory.getLogger(HttpResourceScheduler.class);
 
     public HttpResourceScheduler(KubernetesClient k8sClient) {
         this.k8sClient = k8sClient;
     }
 
     public void schedule(ConfigSource<HttpSourceConfig> configSource) {
+        timer.cancel();
+        final TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                log.info("Updating the {}", configSource);
+                final var configName = configSource.getTargetConfigMapName();
+                final var url = configSource.getSourceConfig().getUrl();
+                final var namespace = configSource.getNamespace();
+
+                try {
+                    final var body = client.send(HttpRequest.newBuilder().uri(URI.create(url)).GET().build(), HttpResponse.BodyHandlers.ofString()).body();
+
+                    final Resource<ConfigMap, DoneableConfigMap> configMapResource = k8sClient.configMaps()
+                            .inNamespace(namespace)
+                            .withName(configName);
 
 
-        final var configName = configSource.getTargetConfigMapName();
-        final var url = configSource.getSourceConfig().getUrl();
-        final var namespace = configSource.getNamespace();
-
-        try {
-            final var body = client.send(HttpRequest.newBuilder().uri(URI.create(url)).GET().build(), HttpResponse.BodyHandlers.ofString()).body();
-
-            final Resource<ConfigMap, DoneableConfigMap> configMapResource = k8sClient.configMaps()
-                    .inNamespace(namespace)
-                    .withName(configName);
-
-
-            ConfigMap configMap = configMapResource.createOrReplace(new ConfigMapBuilder().
-                    withNewMetadata().withName(configName).endMetadata().
-                    addToData("config", body).
-                    build());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+                    ConfigMap configMap = configMapResource.createOrReplace(new ConfigMapBuilder().
+                            withNewMetadata().withName(configName).endMetadata().
+                            addToData("config", body).
+                            build());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        timer.schedule(task, 0, 10_000);
     }
 }
