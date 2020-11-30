fe7ef9f860896260c9e405fd18cbe44d6bd9187b
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2020-11-30 17:24:19 +0100

    ResourceScheduler interface

--- src/main/java/com/javaworm/configme/ResourceScheduler.java
+++ src/main/java/com/javaworm/configme/ResourceScheduler.java
@@ -1,21 +1,7 @@
 package com.javaworm.configme;
 
-import com.javaworm.configme.resources.ConfigSourceResource;
-import io.fabric8.kubernetes.client.KubernetesClient;
+import com.javaworm.configme.sources.HttpSourceConfig;
 
-public class ResourceScheduler {
-    private final ConfigSourceFactory configSourceFactory;
-    private final KubernetesClient k8sClient;
-
-    public ResourceScheduler(ConfigSourceFactory configSourceFactory, KubernetesClient k8sClient) {
-        this.configSourceFactory = configSourceFactory;
-        this.k8sClient = k8sClient;
-    }
-
-    public void schedule(ConfigSourceResource resource) {
-        final var configSource = this.configSourceFactory.create(resource);
-        if (configSource.getSourceType().equals("http")) {
-            new HttpResourceScheduler(k8sClient).schedule(configSource);
-        }
-    }
+public interface ResourceScheduler {
+    void schedule(ConfigSource<HttpSourceConfig> configSource);
 }
