60d7ea3d29dd7e6ba6e03bc7f794538631d1a402
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2020-11-30 13:46:08 +0100

    add namespace to the ConfigSource

--- src/main/java/com/javaworm/configme/ConfigSource.java
+++ src/main/java/com/javaworm/configme/ConfigSource.java
@@ -2,11 +2,13 @@ package com.javaworm.configme;
 
 public class ConfigSource {
     private String sourceType;
+    private String namespace;
     private SourceConfig sourceConfig;
     private String targetConfigMapName;
 
-    public ConfigSource(String sourceType, SourceConfig sourceConfig, String targetConfigMapName) {
+    public ConfigSource(String sourceType, String namespace, SourceConfig sourceConfig, String targetConfigMapName) {
         this.sourceType = sourceType;
+        this.namespace = namespace;
         this.sourceConfig = sourceConfig;
         this.targetConfigMapName = targetConfigMapName;
     }
--- src/main/java/com/javaworm/configme/ConfigSourceFactory.java
+++ src/main/java/com/javaworm/configme/ConfigSourceFactory.java
@@ -14,9 +14,10 @@ public class ConfigSourceFactory {
 
     public ConfigSource create(ConfigSourceResource configSourceResource) {
         final String sourceType = configSourceResource.getSpec().getSourceType();
+        final String namespace = configSourceResource.getMetadata().getNamespace();
         if (sourceType.equals("http")) {
             final HttpSourceConfig httpSourceConfig = this.objectMapper.convertValue(configSourceResource.getSpec().getSourceConfig(), HttpSourceConfig.class);
-            return new ConfigSource(sourceType, httpSourceConfig, configSourceResource.getSpec().getTargetConfigMapName());
+            return new ConfigSource(sourceType, namespace, httpSourceConfig, configSourceResource.getSpec().getTargetConfigMapName());
         }
         throw new RuntimeException(sourceType + " is not supported source type");
     }
