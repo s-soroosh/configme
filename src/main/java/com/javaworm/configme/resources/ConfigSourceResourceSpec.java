59f94c503585c034b9df6869349effc30c0b9a34
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2020-12-02 14:34:07 +0100

    Support field in the code

--- src/main/java/com/javaworm/configme/resources/ConfigSourceResourceSpec.java
+++ src/main/java/com/javaworm/configme/resources/ConfigSourceResourceSpec.java
@@ -6,28 +6,26 @@ public class ConfigSourceResourceSpec {
     private String sourceType;
     private Map sourceConfig;
     private String targetConfigMapName;
+    private boolean blocking;
+    private double blockingTimeoutSeconds;
+
+    public boolean isBlocking() {
+        return blocking;
+    }
+
+    public double getBlockingTimeoutSeconds() {
+        return blockingTimeoutSeconds;
+    }
 
     public String getSourceType() {
         return sourceType;
-    }
-
-    public void setSourceType(String sourceType) {
-        this.sourceType = sourceType;
     }
 
     public Map getSourceConfig() {
         return sourceConfig;
     }
 
-    public void setSourceConfig(Map sourceConfig) {
-        this.sourceConfig = sourceConfig;
-    }
-
     public String getTargetConfigMapName() {
         return targetConfigMapName;
-    }
-
-    public void setTargetConfigMapName(String targetConfigMapName) {
-        this.targetConfigMapName = targetConfigMapName;
     }
 }
