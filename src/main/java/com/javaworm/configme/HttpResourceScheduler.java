4211a0531c7f2ab8dbb0843f62ac309d6c5c5d74
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2020-12-10 22:44:32 +0100

    inject HttpClient

--- src/main/java/com/javaworm/configme/HttpResourceScheduler.java
+++ src/main/java/com/javaworm/configme/HttpResourceScheduler.java
@@ -2,9 +2,6 @@ package com.javaworm.configme;
 
 import com.javaworm.configme.resources.ConfigSourceResource;
 import com.javaworm.configme.sources.HttpSourceConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -14,79 +11,87 @@ import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpResourceScheduler implements ResourceScheduler {
-    private static final Logger log = LoggerFactory.getLogger(HttpResourceScheduler.class);
-    private final HttpClient client = HttpClient
-            .newBuilder()
-            .followRedirects(HttpClient.Redirect.NORMAL)
-            .build();
+  private static final Logger log = LoggerFactory.getLogger(HttpResourceScheduler.class);
+  private final HttpClient client;
 
-    private final FetchedDataHandler fetchedDataHandler;
-    private final Timer timer = new Timer(HttpResourceScheduler.class.getName());
-    private final Map<String, TimerTask> resourceTimers = new ConcurrentHashMap<>();
+  private final FetchedDataHandler fetchedDataHandler;
+  private final Timer timer = new Timer(HttpResourceScheduler.class.getName());
+  private final Map<String, TimerTask> resourceTimers = new ConcurrentHashMap<>();
 
-    public HttpResourceScheduler(FetchedDataHandler fetchedDataHandler) {
-        this.fetchedDataHandler = fetchedDataHandler;
-    }
+  public HttpResourceScheduler(FetchedDataHandler fetchedDataHandler,HttpClient httpClient) {
+    this.fetchedDataHandler = fetchedDataHandler;
+    this.client= httpClient;
+  }
 
-    public void schedule(ConfigSource<HttpSourceConfig> configSource) {
-        cancelCurrentTask(configSource);
-        final var url = configSource.getSourceConfig().getUrl();
-        final var intervalSeconds = configSource.getSourceConfig().getIntervalSeconds();
-        final var intervalMilliseconds = (int) (intervalSeconds * 1000.0);
-        final TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                log.debug("Fetching data from {}", url);
-                final var request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
-                try {
-                    final HttpResponse<String> response;
-                    response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                    if (response.statusCode() < 200 || response.statusCode() > 299) {
-                        log.warn("Fetching data from url {} failed with status code {} and body {}", url, response.statusCode(), response.body());
-                        return;
-                    }
-                    final var body = response.body();
+  public void schedule(ConfigSource<HttpSourceConfig> configSource) {
+    cancelCurrentTask(configSource);
+    final var url = configSource.getSourceConfig().getUrl();
+    final var intervalSeconds = configSource.getSourceConfig().getIntervalSeconds();
+    final var intervalMilliseconds = (int) (intervalSeconds * 1000.0);
+    final TimerTask task =
+        new TimerTask() {
+          @Override
+          public void run() {
+            log.debug("Fetching data from {}", url);
+            final var request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+            try {
+              final HttpResponse<String> response;
+              response = client.send(request, HttpResponse.BodyHandlers.ofString());
+              if (response.statusCode() < 200 || response.statusCode() > 299) {
+                configSource.getContext().emit("Error");
+                log.warn(
+                    "Fetching data from url {} failed with status code {} and body {}",
+                    url,
+                    response.statusCode(),
+                    response.body());
+                return;
+              }
+              final var body = response.body();
 
-                    fetchedDataHandler.handle(configSource, body);
-                } catch (IOException | InterruptedException e) {
-                    log.error("Error in fetching data from " + url, e);
-                }
+              fetchedDataHandler.handle(configSource, body);
+            } catch (IOException | InterruptedException e) {
+              configSource.getContext().emit("Error");
+              log.error("Error in fetching data from " + url, e);
             }
+          }
         };
 
-        timer.schedule(task, 0, intervalMilliseconds);
-        resourceTimers.put(configSource.getUid(), task);
-        log.info("Config source {} scheduled to refresh every {}s", configSource.getName(), intervalSeconds);
-    }
+    timer.schedule(task, 0, intervalMilliseconds);
+    resourceTimers.put(configSource.getUid(), task);
+    log.info(
+        "Config source {} scheduled to refresh every {}s", configSource.getName(), intervalSeconds);
+  }
 
-    private void cancelCurrentTask(ConfigSource<HttpSourceConfig> configSource) {
-        final var uid = configSource.getUid();
-        final var currentTimerTask = resourceTimers.get(uid);
-        if (currentTimerTask == null) {
-            log.info("Resource with UID [{}] has no task yet!", uid);
-            return;
-        }
-        log.info("Canceling task for Resource UID [{}]", uid);
-        final var cancellationResult = currentTimerTask.cancel();
-        log.info("Canceling task result [{}]", cancellationResult);
-        if (cancellationResult) {
-            resourceTimers.remove(uid);
-        }
+  private void cancelCurrentTask(ConfigSource<HttpSourceConfig> configSource) {
+    final var uid = configSource.getUid();
+    final var currentTimerTask = resourceTimers.get(uid);
+    if (currentTimerTask == null) {
+      log.debug("Resource with UID [{}] has no task yet!", uid);
+      return;
     }
+    log.debug("Canceling task for Resource UID [{}]", uid);
+    final var cancellationResult = currentTimerTask.cancel();
+    log.debug("Canceling task result [{}]", cancellationResult);
+    if (cancellationResult) {
+      resourceTimers.remove(uid);
+    }
+  }
 
-    @Override
-    public void cancel(RequestContext<ConfigSourceResource> context) {
-        final var uid = context.getResource().getMetadata().getUid();
-        final var timerTask = resourceTimers.get(uid);
-        if (timerTask != null) {
-            log.info("Canceling task for Resource UID [{}]", uid);
-            final var cancellationResult = timerTask.cancel();
-            log.info("Canceling task result for UID [{}] is [{}]", uid, cancellationResult);
-            if (cancellationResult) {
-                resourceTimers.remove(uid);
-            }
-        }
+  @Override
+  public void cancel(RequestContext<ConfigSourceResource> context) {
+    final var uid = context.getResource().getMetadata().getUid();
+    final var timerTask = resourceTimers.get(uid);
+    if (timerTask != null) {
+      log.debug("Canceling task for Resource UID [{}]", uid);
+      final var cancellationResult = timerTask.cancel();
+      log.debug("Canceling task result for UID [{}] is [{}]", uid, cancellationResult);
+      if (cancellationResult) {
+        resourceTimers.remove(uid);
+      }
     }
+  }
 }
--- src/main/java/com/javaworm/configme/ResourceSchedulerManager.java
+++ src/main/java/com/javaworm/configme/ResourceSchedulerManager.java
@@ -2,39 +2,45 @@ package com.javaworm.configme;
 
 import com.javaworm.configme.resources.ConfigSourceResource;
 import io.fabric8.kubernetes.client.KubernetesClient;
-
+import java.net.http.HttpClient;
 import java.util.Map;
 
 public class ResourceSchedulerManager {
-    private final ConfigSourceFactory configSourceFactory;
+  private final ConfigSourceFactory configSourceFactory;
 
-    private final Map<String, ResourceScheduler> sourceTypeSchedulers;
+  private final Map<String, ResourceScheduler> sourceTypeSchedulers;
 
-    public ResourceSchedulerManager(ConfigSourceFactory configSourceFactory, KubernetesClient k8sClient, FetchedDataHandler fetchedDataHandler) {
-        this.configSourceFactory = configSourceFactory;
-        sourceTypeSchedulers = Map.of(
-                "http", new HttpResourceScheduler(fetchedDataHandler)
-        );
+  public ResourceSchedulerManager(
+      ConfigSourceFactory configSourceFactory,
+      KubernetesClient k8sClient,
+      FetchedDataHandler fetchedDataHandler) {
+    this.configSourceFactory = configSourceFactory;
+    sourceTypeSchedulers =
+        Map.of(
+            "http",
+            new HttpResourceScheduler(
+                fetchedDataHandler,
+                HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build()));
+  }
+
+  public void schedule(RequestContext<ConfigSourceResource> context) {
+    cancelExistingScheduleFor(context);
+    final var configSource = this.configSourceFactory.create(context);
+    final var sourceType = configSource.getSourceType();
+    final var scheduler = sourceTypeSchedulers.get(sourceType);
+    if (scheduler == null) {
+      final var errorMessage = String.format("No scheduler found for source type [%s]", sourceType);
+      context.emit(errorMessage);
+      throw new RuntimeException(errorMessage);
     }
+    scheduler.schedule(configSource);
+  }
 
-    public void schedule(RequestContext<ConfigSourceResource> context) {
-        cancelExistingScheduleFor(context);
-        final var configSource = this.configSourceFactory.create(context);
-        final var sourceType = configSource.getSourceType();
-        final var scheduler = sourceTypeSchedulers.get(sourceType);
-        if (scheduler == null) {
-            final var errorMessage = String.format("No scheduler found for source type [%s]", sourceType);
-            context.emit(errorMessage);
-            throw new RuntimeException(errorMessage);
-        }
-        scheduler.schedule(configSource);
-    }
+  public void cleanup(RequestContext<ConfigSourceResource> context) {
+    cancelExistingScheduleFor(context);
+  }
 
-    public void cleanup(RequestContext<ConfigSourceResource> context) {
-        cancelExistingScheduleFor(context);
-    }
-
-    private void cancelExistingScheduleFor(RequestContext<ConfigSourceResource> context) {
-        sourceTypeSchedulers.values().forEach(scheduler -> scheduler.cancel(context));
-    }
+  private void cancelExistingScheduleFor(RequestContext<ConfigSourceResource> context) {
+    sourceTypeSchedulers.values().forEach(scheduler -> scheduler.cancel(context));
+  }
 }
