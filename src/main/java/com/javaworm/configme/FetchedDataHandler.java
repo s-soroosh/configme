2005e3f075ff091ffe0702764ed33f0e505f71e4
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2021-01-11 23:16:28 +0100

    gzip implementation

--- src/main/java/com/javaworm/configme/FetchedDataHandler.java
+++ src/main/java/com/javaworm/configme/FetchedDataHandler.java
@@ -8,10 +8,16 @@ import io.fabric8.kubernetes.api.model.DoneableConfigMap;
 import io.fabric8.kubernetes.api.model.OwnerReference;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.Resource;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 import java.util.Date;
+import java.util.Optional;
+import java.util.zip.GZIPOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipUtils;
 
 public class FetchedDataHandler {
   private final KubernetesClient k8sClient;
@@ -22,7 +28,8 @@ public class FetchedDataHandler {
     this.k8sEventEmitter = new K8sEventEmitter(k8sClient);
   }
 
-  public void handle(ConfigSource<? extends SourceConfig> configSource, String data) {
+  public void handle(ConfigSource<? extends SourceConfig> configSource, String data)
+      throws IOException {
     final var namespace = configSource.getNamespace();
     final var configName = configSource.getTargetConfigMapName();
     final String eventTime =
@@ -33,9 +40,37 @@ public class FetchedDataHandler {
         k8sClient.configMaps().inNamespace(namespace).withName(configName);
     final ConfigSourceEvent event;
     final var existingConfigmap = configMapResource.get();
-    if (existingConfigmap == null) {
-      ConfigMap configMap =
-          configMapResource.createOrReplace(
+    final var provideAs =
+        Optional.ofNullable(configSource.getResource().getSpec().getProvideAs()).orElse("is");
+    System.out.println("provide as " + provideAs);
+    if (provideAs.equals("is")) {
+      if (existingConfigmap == null) {
+        ConfigMap configMap =
+            configMapResource.createOrReplace(
+                new ConfigMapBuilder()
+                    .withNewMetadata()
+                    .withName(configName)
+                    .withOwnerReferences(
+                        new OwnerReference(
+                            configSource.getResource().getApiVersion(),
+                            true,
+                            true,
+                            configSource.getResource().getKind(),
+                            configSource.getName(),
+                            configSource.getUid()))
+                    .endMetadata()
+                    .addToData("config", data)
+                    .addToData("lastUpdate", new Date().toString())
+                    .addToData("hash", String.valueOf(data.hashCode()))
+                    .build());
+        event =
+            new ConfigSourceEvent(
+                "UPDATE", "dataLoaded", String.format("Configmap %s has been created", configName));
+      } else {
+        final String existingHash = existingConfigmap.getData().get("hash");
+        final String newHash = String.valueOf(data.hashCode());
+        if (!newHash.equals(existingHash)) {
+          configMapResource.replace(
               new ConfigMapBuilder()
                   .withNewMetadata()
                   .withName(configName)
@@ -52,43 +87,99 @@ public class FetchedDataHandler {
                   .addToData("lastUpdate", new Date().toString())
                   .addToData("hash", String.valueOf(data.hashCode()))
                   .build());
-      event =
-          new ConfigSourceEvent(
-              "UPDATE", "dataLoaded", String.format("Configmap %s has been created", configName));
+          event =
+              new ConfigSourceEvent(
+                  "UPDATE",
+                  "dataLoaded",
+                  String.format("Configmap %s has been updated", configName));
+        } else {
+          event =
+              new ConfigSourceEvent(
+                  "UPDATE",
+                  "dataLoaded",
+                  String.format("Configmap %s is up to date, left intact", configName));
+        }
+      }
+      this.k8sEventEmitter.emit(configSource.getResource(), event);
 
-    } else {
-      final String existingHash = existingConfigmap.getData().get("hash");
-      final String newHash = String.valueOf(data.hashCode());
-      if (!newHash.equals(existingHash)) {
-        configMapResource.replace(
-            new ConfigMapBuilder()
-                .withNewMetadata()
-                .withName(configName)
-                .withOwnerReferences(
-                    new OwnerReference(
-                        configSource.getResource().getApiVersion(),
-                        true,
-                        true,
-                        configSource.getResource().getKind(),
-                        configSource.getName(),
-                        configSource.getUid()))
-                .endMetadata()
-                .addToData("config", data)
-                .addToData("lastUpdate", new Date().toString())
-                .addToData("hash", String.valueOf(data.hashCode()))
-                .build());
+    } else if (provideAs.equals("gzip")) {
+      if (existingConfigmap == null) {
+        byte[] compressedData = compress(data);
+        final var encodedCompressedData = Base64.getEncoder().encodeToString(compressedData);
+        ConfigMap configMap =
+            configMapResource.createOrReplace(
+                new ConfigMapBuilder()
+                    .withNewMetadata()
+                    .withName(configName)
+                    .withOwnerReferences(
+                        new OwnerReference(
+                            configSource.getResource().getApiVersion(),
+                            true,
+                            true,
+                            configSource.getResource().getKind(),
+                            configSource.getName(),
+                            configSource.getUid()))
+                    .endMetadata()
+                    .addToBinaryData("config", encodedCompressedData)
+                    .addToData("lastUpdate", new Date().toString())
+                    .addToData("hash", String.valueOf(data.hashCode()))
+                    .build());
         event =
             new ConfigSourceEvent(
-                "UPDATE", "dataLoaded", String.format("Configmap %s has been updated", configName));
+                "UPDATE", "dataLoaded", String.format("Configmap %s has been created", configName));
+        this.k8sEventEmitter.emit(configSource.getResource(), event);
+
       } else {
-        event =
-            new ConfigSourceEvent(
-                "UPDATE",
-                "dataLoaded",
-                String.format("Configmap %s is up to date, left intact", configName));
+        final String existingHash = existingConfigmap.getData().get("hash");
+        final String newHash = String.valueOf(data.hashCode());
+        try {
+          byte[] compressedData = compress(data);
+          final var encodedCompressedData = Base64.getEncoder().encodeToString(compressedData);
+          if (!newHash.equals(existingHash)) {
+            configMapResource.replace(
+                new ConfigMapBuilder()
+                    .withNewMetadata()
+                    .withName(configName)
+                    .withOwnerReferences(
+                        new OwnerReference(
+                            configSource.getResource().getApiVersion(),
+                            true,
+                            true,
+                            configSource.getResource().getKind(),
+                            configSource.getName(),
+                            configSource.getUid()))
+                    .endMetadata()
+                    .addToBinaryData("config", encodedCompressedData)
+                    .addToData("lastUpdate", new Date().toString())
+                    .addToData("hash", String.valueOf(data.hashCode()))
+                    .build());
+            event =
+                new ConfigSourceEvent(
+                    "UPDATE",
+                    "dataLoaded",
+                    String.format("Configmap %s has been updated", configName));
+          } else {
+            event =
+                new ConfigSourceEvent(
+                    "UPDATE",
+                    "dataLoaded",
+                    String.format("Configmap %s is up to date, left intact", configName));
+          }
+          this.k8sEventEmitter.emit(configSource.getResource(), event);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
       }
     }
+  }
 
-    this.k8sEventEmitter.emit(configSource.getResource(), event);
+  private byte[] compress(String data) throws IOException {
+    ByteArrayOutputStream bos = new ByteArrayOutputStream(data.length());
+    GZIPOutputStream gzip = new GZIPOutputStream(bos);
+    gzip.write(data.getBytes());
+    gzip.close();
+    byte[] compressed = bos.toByteArray();
+    bos.close();
+    return compressed;
   }
 }
